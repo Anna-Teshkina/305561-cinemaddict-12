9b46b4a25dffeee8a0adff1e0b317b9edb36a418
Anna Teshkina <anna_teshkina@mail.ru>
2020-08-21 03:57:20 +0300

    Генерируем массив моков на основе созданной структуры карточки фильма

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ import {createUserProfileTemplate} from "./view/user-profile.js";
 import {createMenuTemplate} from "./view/menu.js";
 import {createSortTemplate} from "./view/sort.js";
 import {createBoardTemplate} from "./view/board.js";
-import {createExtraBoardTemplate} from "./view/extra-board.js";
+// import {createExtraBoardTemplate} from "./view/extra-board.js";
 import {createShowBtnTemplate} from "./view/show-btn.js";
 import {createCardTemplate} from "./view/card.js";
 // import {createPopupTemplate} from "./view/popup.js";
 import {createFooterStatisticTemplate} from "./view/footer-statistic.js";
 
+import {generateFilm} from "./mock/film.js";
+
+
 // 2. Объявление констант
 const CARD_COUNT = 5;
-const CARD_FOOTER_COUNT = 2;
-const EXTRA_BOARD_TITLES = [`Top rated`, `Most commented`];
+// const CARD_FOOTER_COUNT = 2;
+// const EXTRA_BOARD_TITLES = [`Top rated`, `Most commented`];
 
 // 3. Объявление переменных, значение которых известно до начала работы программы
 
@@ -21,6 +24,10 @@ const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
 
 const footerStatisticElement = siteFooterElement.querySelector(`.footer__statistics`);
+
+// ПЕРЕНЕСТИ
+const films = new Array(CARD_COUNT).fill().map(generateFilm);
+// console.log(films);
 
 
 // 4. Объявление функций
@@ -44,7 +51,7 @@ render(siteMainElement, createSortTemplate(), `beforeend`);
 // - отрисовка компоненты доски
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const mainBoardElement = document.querySelector(`.films`);
+// const mainBoardElement = document.querySelector(`.films`);
 const mainBoardListElement = document.querySelector(`.films-list .films-list__container`);
 
 // - отрисовка кнопки загрузки
@@ -52,21 +59,21 @@ render(mainBoardListElement, createShowBtnTemplate(), `afterend`);
 
 // - отрисовка 5 карточек на основную доску
 for (let i = 0; i < CARD_COUNT; i++) {
-  render(mainBoardListElement, createCardTemplate(), `beforeend`);
+  render(mainBoardListElement, createCardTemplate(films[i]), `beforeend`);
 }
 
 // - отрисовка 2 extra блоков для фильмов с высоким рейтингом и наиболее обсуждаемых
-for (let i = 0; i < EXTRA_BOARD_TITLES.length; i++) {
-  render(mainBoardElement, createExtraBoardTemplate(EXTRA_BOARD_TITLES[i]), `beforeend`);
-}
+// for (let i = 0; i < EXTRA_BOARD_TITLES.length; i++) {
+//   render(mainBoardElement, createExtraBoardTemplate(EXTRA_BOARD_TITLES[i]), `beforeend`);
+// }
 
-const extraFilmContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
+// const extraFilmContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
 
-Array.from(extraFilmContainers).forEach(function (item) {
-  for (let i = 0; i < CARD_FOOTER_COUNT; i++) {
-    render(item, createCardTemplate(), `beforeend`);
-  }
-});
+// Array.from(extraFilmContainers).forEach(function (item) {
+//   for (let i = 0; i < CARD_FOOTER_COUNT; i++) {
+//     render(item, createCardTemplate(), `beforeend`);
+//   }
+// });
 
 // - отрисовка статистики в подвале сайта
 render(footerStatisticElement, createFooterStatisticTemplate(), `beforeend`);
