02bc939543e142c6c22cb4b648f971d043295497
Anna Teshkina <anna_teshkina@mail.ru>
2020-08-22 12:11:33 +0300

    Добавила вывод флагов в шаблон: в листе просмотра, просмотрено, избранное

--- src/view/card.js
+++ src/view/card.js
@@ -1,13 +1,13 @@
 // - шаблон карточки фильма
 export const createCardTemplate = (film) => {
-  const {name, raiting, year, duration, genre, poster, shortDescription, commentsCount, isInWatchlist, isWatched, isFavorite} = film;
+  const {name, raiting, year, duration, genre, poster, shortDescription, commentsCount, watchlist, watched, favorite} = film;
 
   // устанавливаем  активный класс на кнопку в зависимости от переданного флага
   const setActiveClassName = (flag) => flag ? `film-card__controls-item--active` : ``;
 
   // для карточки фильма указываем в качестве жанра первый из списка
-  const gengesList = genre.split(`,`).map((item) => item.trim());
-  const filmGenre = gengesList[0];
+  const genresList = genre.split(`,`).map((item) => item.trim());
+  const filmGenre = genresList[0];
 
   return `<article class="film-card">
     <h3 class="film-card__title">${name}</h3>
@@ -21,9 +21,9 @@ export const createCardTemplate = (film) => {
     <p class="film-card__description">${shortDescription}</p>
     <a class="film-card__comments">${commentsCount} comments</a>
     <form class="film-card__controls">
-      <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${setActiveClassName(isInWatchlist)}">Add to watchlist</button>
-      <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${setActiveClassName(isWatched)}">Mark as watched</button>
-      <button class="film-card__controls-item button film-card__controls-item--favorite ${setActiveClassName(isFavorite)}">Mark as favorite</button>
+      <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${setActiveClassName(watchlist)}">Add to watchlist</button>
+      <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${setActiveClassName(watched)}">Mark as watched</button>
+      <button class="film-card__controls-item button film-card__controls-item--favorite ${setActiveClassName(favorite)}">Mark as favorite</button>
     </form>
   </article>`;
 };
