a2b36a0bc1aa10e210b01c4c4a17f800e5d21951
Anna Teshkina <anna_teshkina@mail.ru>
2020-09-12 13:34:41 +0300

    Правим шаблон сортировки: -добавляем data-атрибуты, чтобы при клике можно было определить, какой тип сортировки выбрал пользователь, - все возможные типы сортировки описываем в константе.

--- src/const.js
+++ src/const.js
@@ -2,3 +2,9 @@
 
 export const EMOJIES = [`smile`, `sleeping`, `puke`, `angry`];
 export const ESC_CODE = 27;
+
+export const SortType = {
+  DEFAULT: `default`,
+  DATE: `by-date`,
+  RAITING: `by-raiting`
+};
