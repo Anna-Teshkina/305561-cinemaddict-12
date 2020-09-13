dad617059cd26e519bd9cdd0a6b5b901d49f0b50
Anna Teshkina <anna_teshkina@mail.ru>
2020-09-13 16:34:27 +0300

    Описываем логику сортировки.

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,5 @@
+import {MONTHS} from "../const.js";
+
 // Функция по генерации ЦЕЛОГО случайного числа из диапазона
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -46,3 +48,51 @@ export const getRandomString = (array, seperator, minCount, maxCount, flag = tru
 // Функция делает первую букву строки заглавной
 export const convertFirstLetterToUppercase = (word) =>
   word.slice(0, 1).toUpperCase() + word.slice(1, word.length);
+
+
+const convertToNumberDate = (dateArray) => {
+  const newDateArray = [];
+
+  dateArray.forEach((item) => {
+    const flag = MONTHS.indexOf(item);
+    if (flag === -1) {
+      newDateArray.push(parseInt(item, 10));
+    } else {
+      newDateArray.push(MONTHS.indexOf(item) + 1);
+    }
+  });
+  return newDateArray;
+};
+
+const compare = (a, b) => {
+  if (a > b) {
+    return -1;
+  }
+  if (a < b) {
+    return 1;
+  }
+  return 0;
+};
+
+export const sortDate = (cardA, cardB) => {
+  // переведем дату из формата 18 October 1965 в массив [1965, 10, 18]
+  // и сравним даты сначала по году, потом по месяцу и по дате
+  const dateFirstArray = convertToNumberDate(cardA.release.split(` `).reverse());
+  const dateSecondArray = convertToNumberDate(cardB.release.split(` `).reverse());
+
+  const stepLength = dateFirstArray.length;
+
+  let weight;
+  for (let i = 0; i < stepLength; i++) {
+    weight = compare(dateFirstArray[i], dateSecondArray[i]);
+    if (weight !== 0) {
+      break;
+    }
+  }
+
+  return weight;
+};
+
+export const sortRaiting = (cardA, cardB) => {
+  return cardB.raiting - cardA.raiting;
+};
